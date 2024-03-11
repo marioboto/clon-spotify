2c536bd8e60b5f66025aabd289cad1ecbf6846c9
marioboto <mboto.professional@gmail.com>
2024-03-11 13:54:44 +0100

    updated player and main

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,22 @@
+import { Pause, Play } from "./Player"
+import { userPlayerStore } from "@/store/playerStore"
+
+export function CardPlayButton ({ id }) {
+    const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic} = userPlayerStore(state => state)
+    
+    const handleClick = () => {
+        setCurrentMusic({
+            playlist: {
+                id
+            }
+        })
+        setIsPlaying(!isPlaying)
+    }
+
+    const isPlayingPlaylist = isPlaying &&  currentMusic?.playlist.id == id
+    return (
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+           {isPlayingPlaylist ? <Pause /> : <Play />}
+        </button>
+    )
+}
