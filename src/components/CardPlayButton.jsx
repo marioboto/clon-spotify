48bd3f35fdc9d6b5833036e248a77398992ea6b2
marioboto <mboto.professional@gmail.com>
2024-03-13 13:15:21 +0100

    updated player

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -3,17 +3,22 @@ import { userPlayerStore } from "@/store/playerStore"
 
 export function CardPlayButton ({ id }) {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic} = userPlayerStore(state => state)
+    const isPlayingPlaylist = isPlaying &&  currentMusic?.playlist.id == id
     
     const handleClick = () => {
-        setCurrentMusic({
-            playlist: {
-                id
-            }
+        if (isPlayingPlaylist){
+            setIsPlaying(false)
+            return
+        }
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const {songs, playlist} = data
+            setIsPlaying(true)
+            setCurrentMusic({songs, playlist, song: songs[0]})
         })
-        setIsPlaying(!isPlaying)
     }
 
-    const isPlayingPlaylist = isPlaying &&  currentMusic?.playlist.id == id
     return (
         <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
            {isPlayingPlaylist ? <Pause /> : <Play />}
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export const userPlayerStore = create((set) => ({
     isPlaying: false,
     currentMusic: { playlist: null, song: null, songs: []},
+    volume: 1,
+    setVolume: (volume) => set({volume}),
     setIsPlaying: (isPlaying) => set({isPlaying}),
     setCurrentMusic: (currentMusic) => set({currentMusic})
 }));
