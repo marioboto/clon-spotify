1f640a92465f75d72d05fce34b0db7bbdf434091
marioboto <mboto.professional@gmail.com>
2024-03-14 13:13:34 +0100

    finished copy clon

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from "./Player"
 import { userPlayerStore } from "@/store/playerStore"
 
-export function CardPlayButton ({ id }) {
+export function CardPlayButton ({ id, size = 'small' }) {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic} = userPlayerStore(state => state)
     const isPlayingPlaylist = isPlaying &&  currentMusic?.playlist.id == id
     
@@ -19,9 +19,11 @@ export function CardPlayButton ({ id }) {
         })
     }
 
+    const iconClassName = size == 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
     return (
-        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-           {isPlayingPlaylist ? <Pause /> : <Play />}
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+           {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
         </button>
     )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -79,6 +79,51 @@ const VolumeControl = () => {
     )
 }
 
+const SongControl = ({ audio }) => {
+    const [currentTime, setCurrentTime] = useState(0)
+
+    useEffect(() => {
+        audio.current.addEventListener('timeupdate', handleTimeUpdate)
+        return () => {
+            audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+        }
+    }, [])
+
+    const handleTimeUpdate = () => {
+        setCurrentTime(audio.current.currentTime)
+    }
+
+    const formatTime = time => {
+        if (time == null) return `0:00`
+        const seconds = Math.floor(time % 60)
+        const minutes = Math.floor(time / 60)
+
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+    const duration = audio?.current?.duration ?? 0
+
+    return (
+        <div className="flex gap-x-3 text-xs pt-2">
+            <span className="opacity-50 w-12 text-right">{formatTime(currentTime)}</span>
+
+            <Slider
+                defaultValue={[0]}
+                max={audio?.current?.duration ?? 0}
+                min={0}
+                value={[currentTime]}
+                className="w-[400px]"
+                onValueChange={(value) => {
+                    const [newCurrentTime] = value
+                    audio.current.currentTime = newCurrentTime
+                    
+                }} />
+
+            <span className="opacity-50 w-12">
+                {duration ? formatTime(duration) : '0:00'}</span>
+        </div>
+    )
+}
+
 export function Player() {
     const { currentMusic, isPlaying, setIsPlaying, volume } = userPlayerStore(state => state)
     const audioRef = useRef()
@@ -108,15 +153,16 @@ export function Player() {
     }
 
     return (
-        <div className="flex flex-row justify-between w-full px-4 z-50">
-            <div>
+        <div className="flex flex-row justify-between w-full px-1 z-50">
+            <div className="w-[200px]">
                 <CurrentSong {...currentMusic.song} />
             </div>
             <div className="grid place-content-center gap-4 flex-1">
-                <div className="flex justify-center">
+                <div className="flex justify-center flex-col items-center">
                     <button className="bg-white rounded-full p-2" onClick={handleClick}>
                         {isPlaying ? <Pause /> : <Play />}
                     </button>
+                    <SongControl audio={audioRef} />
                     <audio ref={audioRef} />
                 </div>
             </div>
